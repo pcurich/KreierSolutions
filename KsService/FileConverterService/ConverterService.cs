0f83c50142b276044a4e6c19ea3c44a684b686ee
Pedro Curich Gonzales <curichpedro@gmail.com>
2016-10-26 08:12:49 -0500

    41

---
+++ KsService/FileConverterService/ConverterService.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Xml.Schema;
+
+namespace FileConverterService
+{
+    public class ConverterService
+    {
+        private FileSystemWatcher _watcher;
+
+        public bool Start()
+        {
+            _watcher = new FileSystemWatcher(@"D:\KsService", "*_in.txt");
+            _watcher.Created += FileCreated;
+            _watcher.IncludeSubdirectories = false;
+            _watcher.EnableRaisingEvents = true;
+            return true;
+        }
+
+        private void FileCreated(object sender , FileSystemEventArgs e)
+        {
+            var content = File.ReadAllText(e.FullPath);
+            var upperContent = content.ToUpperInvariant();
+            var dir = Path.GetDirectoryName(e.FullPath);
+            var convertedFileName = Path.GetFileName(e.FullPath) + ".converted";
+            if (dir != null)
+            {
+                var convertedPath = Path.Combine(dir, convertedFileName);
+                File.WriteAllText(convertedPath,upperContent);
+            }
+        }
+
+        public bool Stop()
+        {
+            _watcher.Dispose();
+            return true;
+        }
+    }
+}
---
+++ KsService/FileConverterService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace FileConverterService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.Service<ConverterService>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(() => new ConverterService());
+                    serviceInstance.WhenStarted(execute => execute.Start());
+                    serviceInstance.WhenStopped(execute => execute.Stop());
+                });
+
+                serviceConfig.SetServiceName("AwesomeFileConverted");
+                serviceConfig.SetDisplayName("Awesome File Converted");
+                serviceConfig.SetDescription("A pluralsight demo service");
+
+                serviceConfig.StartAutomatically();
+
+            });
+        }
+    }
+}
