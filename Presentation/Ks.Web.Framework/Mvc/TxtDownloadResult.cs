b98b1c462b5a36ce01e92ec2697c052648bf4f2f
Pedro Curich Gonzales <curichpedro@gmail.com>
2017-02-02 09:01:30 -0500

    110

---
+++ Presentation/Ks.Web.Framework/Mvc/TxtDownloadResult.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace Ks.Web.Framework.Mvc
+{
+    public class TxtDownloadResult: ActionResult
+    {
+        public TxtDownloadResult(string txt, string fileDownloadName)
+        {
+            Txt = txt;
+            FileDownloadName = fileDownloadName;
+        }
+
+        public string FileDownloadName{get;set;}
+
+        public string Txt { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var sb = new StringBuilder();
+            sb.Append(Txt);
+
+            context.HttpContext.Response.Charset = "utf-8";
+            context.HttpContext.Response.ContentType = "text/plain";
+            context.HttpContext.Response.AddHeader("content-disposition", string.Format("attachment; filename={0}", FileDownloadName));
+            context.HttpContext.Response.BinaryWrite(Encoding.UTF8.GetBytes(sb.ToString()));
+            context.HttpContext.Response.End();
+        }
+    }
+}
---
+++ Presentation/Ks.Web/Administration/Models/Batchs/ScheduleBatchListModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Ks.Admin.Models.Batchs
+{
+    public class ScheduleBatchListModel
+    {
+        public ScheduleBatchListModel()
+        {
+            Sources =new List<SelectListItem>();
+            Months = new List<SelectListItem>();
+            Years= new List<SelectListItem>();
+        }
+
+        public int Source { get; set; }
+        public List<SelectListItem> Sources { get; set; }
+        public int MonthId { get; set; }
+        public List<SelectListItem> Months { get; set; }
+        public int YearId { get; set; }
+        public List<SelectListItem> Years { get; set; }
+    }
+}
