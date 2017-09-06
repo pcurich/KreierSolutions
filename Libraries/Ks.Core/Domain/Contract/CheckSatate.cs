75e76c0d4b5b90c13e6b10caaeda6a4279de6c44
Pedro Curich Gonzales <curichpedro@gmail.com>
2017-09-06 12:24:23 -0500

    1200

---
+++ Libraries/Ks.Core/Domain/Contract/CheckSatate.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Ks.Core.Domain.Contract
+{
+    public enum CheckSatate
+    {
+        [Description("Vigente")]
+        Active = 1,
+        [Description("Anulado")]
+        Cancel = 2, 
+    }
+}
---
+++ Libraries/Ks.Core/Domain/Contract/EntityTypeValues.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Ks.Core.Domain.Contract
+{
+    public enum EntityTypeValues
+    {
+        [Description("Apoyo Social Económico")]
+        Loan=1,
+        [Description("Devolución")]
+        Return=2,
+        [Description("Beneficio")]
+        Benefit=3
+    }
+}
