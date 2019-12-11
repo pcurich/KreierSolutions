371cce232cda2d394367ec48504c6af826688306
Pedro Curich <curichpedro@gmail.com>
2019-12-11 01:09:13 -0500

    M

--- WindowsService/Ks.Batch.Printer/ShoppingService.cs
+++ WindowsService/Ks.Batch.Printer/ShoppingService.cs
@@ -1,29 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using Newtonsoft.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ks.Batch.Printer
 {
-    public class ShoppingService
+    public class ShoppingService: JsonSerializer
     {
         public class ShoppingCart
         {
+            [JsonProperty("_id")]
+            public string Id { get; set; }
+
+            [JsonProperty("ticketNumber")]
             public int TicketNumber { get; set; }
-            public string UserName { get; set; }
-            public string Customer { get; set; }
+            
+            [JsonProperty("cash")]
+            public double Cash { get; set; }
+            
+            [JsonProperty("credit")]
+            public double Credit { get; set; }
+
+            [JsonProperty("change")]
+            public double Change { get; set; }
+
+            [JsonProperty("paymentType")] 
+            public string PaymentType { get; set;}
+            
+            [JsonProperty("total")]
+            public double Total { get; set; } 
+            public Customer Customer { get; set; }
+            public User User { get; set; }
             public List<Details> Details { get; set; }
-            public double Total { get; set; }
+            
         }
 
-        public class Details
+        public class Details: JsonSerializer
         {
             public double Quantity { get; set; }
-            public string ProductName { get; set; }
+            public Product Product { get; set; }
             public double Price { get; set; }
             public double SubTotal { get { return Price * Quantity; } }
 
         }
+
+        public class Customer: JsonSerializer
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            
+            [JsonProperty("last_name")] 
+            public string LastName { get; set; }
+        }
+
+        public class User :JsonSerializer
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+        }
+
+        public class Product : JsonSerializer
+        {
+            [JsonProperty("long_name")]
+            public string Name { get; set; }
+        }
+
+        public static ShoppingCart LoadJson( string fullPath)
+        {
+            ShoppingCart jsonData = null;
+            try
+            {
+                using (StreamReader r = new StreamReader(fullPath))
+                {
+                    var json = r.ReadToEnd();
+                    json = json.Replace("\\", "");
+                    jsonData = JsonConvert.DeserializeObject<ShoppingCart>(json);
+                }
+            }catch(Exception e)
+            {
+                var error = e.Message;
             }
+            return jsonData;
+        }
+    }
 }
--- WindowsService/Ks.Batch.Printer/Watcher.cs
+++ WindowsService/Ks.Batch.Printer/Watcher.cs
@@ -4,18 +4,42 @@ using Topshelf.Logging;
 using System.Drawing.Printing;
 using System.Drawing;
 using System;
+using static Ks.Batch.Printer.ShoppingService;
 
 namespace Ks.Batch.Printer
 {
     public class Watcher
     {
         private static readonly LogWriter Log = HostLogger.Get<Watcher>();
-
+        //public ShoppingCart jsonNode = new ShoppingCart();
         public static void FileCreated(object sender, FileSystemEventArgs e)
         {
             Thread.Sleep(1000 * 2); //10 Sec because is not atomic
 
-            var jsonNode = ShoppingService.LoadJson();
+            //jsonNode = LoadJson(e.FullPath);
+
+            PrintDocument pd = new PrintDocument();
+            PaperSize ps = new PaperSize("", 475, 550);
+
+            pd.PrintPage += (sender2, args) => pd_PrintPage(e.FullPath, args); 
+
+            pd.PrintController = new StandardPrintController();
+            pd.DefaultPageSettings.Margins.Left = 0;
+            pd.DefaultPageSettings.Margins.Right = 0;
+            pd.DefaultPageSettings.Margins.Top = 0;
+            pd.DefaultPageSettings.Margins.Bottom = 0;
+
+            pd.DefaultPageSettings.PaperSize = ps;
+            pd.Print();
+
+            MoveFile(e.FullPath, e.Name);
+
+
+        }
+
+        public static void pd_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            var jsonNode = LoadJson(sender.ToString());
 
             int BODYFONTSISE = 6;
             int FONTSISE = 8;
@@ -31,9 +55,9 @@ namespace Ks.Batch.Printer
 
             string title = @"c:/print/CBT_Title.png";
             //string barcode = Application.StartupPath + "\\code128bar.jpg";
-            //Graphics g = e.Graphics;
-            Bitmap myBitmap = new Bitmap(@"c:/print/background.png");
-            Graphics g = Graphics.FromImage(myBitmap);
+            Graphics g = e.Graphics;
+            //Bitmap myBitmap = new Bitmap(@"c:/print/background.png");
+            //Graphics g = Graphics.FromImage(myBitmap);
 
             g.DrawRectangle(Pens.Black, 0, 0, PAPER_WIDTH, PAPER_HEIGTH);
 
@@ -55,9 +79,9 @@ namespace Ks.Batch.Printer
             g.DrawString("Av. Arenales 1279".ToCenter(), fBody1, sb, 0, LOGO_HEIGTH + STEP);
             g.DrawString("Fecha:".ToExtreme(DateTime.Now.ToShortDateString()), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * 2);
             g.DrawString("Hora:".ToExtreme(DateTime.Now.ToLongTimeString()), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * 3);
-            g.DrawString("Barista:".ToExtreme("Darwin Fernández"), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * 4);
+            g.DrawString("Barista:".ToExtreme(jsonNode.User.Name), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * 4);
             g.DrawString("Ticket Nro:".ToExtreme(jsonNode.TicketNumber.ToString("D7")), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * 5);
-            g.DrawString("Cliente:".ToExtreme(jsonNode.Customer), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * 6);
+            g.DrawString("Cliente:".ToExtreme(jsonNode.Customer != null ? jsonNode.Customer.Name + " " + jsonNode.Customer.LastName : ""), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * 6);
             g.DrawString("-".Continuous(), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * 7);
             g.DrawString(" ".Continuous(), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * 8);
             g.DrawString("".GetHeaderDetail(), fBody, sb, 0, LOGO_HEIGTH + BLANK + STEP * 9);
@@ -66,7 +90,7 @@ namespace Ks.Batch.Printer
             var i = 11;
             foreach (var p in jsonNode.Details)
             {
-                g.DrawString((p.Quantity + " " + p.ProductName).TrimEnd().ToExtreme(p.Price.ToString("C").Replace("S/", "").ToString().Trim()), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * i);
+                g.DrawString((p.Quantity + " " + p.Product.Name).TrimEnd().ToExtreme(p.Price.ToString("C").Replace("S/", "").ToString().Trim()), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * i);
                 i++;
             }
 
@@ -75,9 +99,22 @@ namespace Ks.Batch.Printer
             g.DrawString(("Igv: " + (jsonNode.Total - jsonNode.Total * 0.82).ToString("C")).ToRight(), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * (i) + STEP * 2);
             g.DrawString(("Total: " + jsonNode.Total.ToString("C2")).ToRight(), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * (i) + STEP * 3);
             g.DrawString("-".Continuous(), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * (i) + STEP * 4);
-            g.DrawString("Gracias Por su Preferencia".ToCenter(), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * (i) + STEP * 5);
+
+            g.DrawString(("Método: " + (jsonNode.PaymentType == "Cash" ? "Efectivo" : "Tarjeta")).ToRight(), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * (i) + STEP * 5);
+            g.DrawString(("Entregado: " + (jsonNode.PaymentType == "Cash" ? jsonNode.Cash : jsonNode.Credit).ToString("C")).ToRight(), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * (i) + STEP * 6);
+            g.DrawString(("Vuelto: " + (Math.Abs(jsonNode.Change).ToString("C"))).ToRight(), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * (i) + STEP * 7);
+            g.DrawString("-".Continuous(), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * (i) + STEP * 8);
+            g.DrawString("Gracias Por su Preferencia".ToCenter(), fBody1, sb, 0, LOGO_HEIGTH + BLANK + STEP * (i) + STEP * 9);
 
             //g.DrawString(TType, fTType, sb, 0, LOGO_HEIGTH + BLANK + STEP * (i) + STEP * 6);
+        }
+
+        private static void MoveFile(string fullPath, string fileName)
+        {
+            var destiny = Path.Combine(Path.Combine(@"c:/print/queve", "done"), fileName);
+            if (File.Exists(destiny))
+                File.Delete(destiny);
+            File.Move(fullPath, Path.Combine(Path.Combine(@"c:/print/queve", "done"), fileName));
         }
     }
 }
